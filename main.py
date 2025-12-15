cdc85c84c04cae043fddc6e2f8c092b38098f302
rishikabilla <rishika.billa04@gmail.com>
2025-12-15 11:49:08 +0530

    Added weight conversion functionality

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def length():
                             "foot": 0.3048,
                             "yard": 0.9144,
                             "mile": 1609.34}
-        length=int(request.form.get("length"))
+        length=float(request.form.get("length"))
         from_unit=request.form["from"]
         to_unit=request.form["to"]
         # Using meter as the base unit allows conversion between any two units
@@ -28,6 +28,21 @@ def length():
 def weight():
     if request.method=="GET":
         return render_template("weight.html")
+    if request.method=="POST":
+        # All length units converted to gram (base unit) for easy and scalable conversion
+        weight_factors = {"mg": 0.001,      
+                        "g": 1,         
+                        "kg": 1000,       
+                        "oz": 28.3495,    
+                        "lb": 453.592}
+
+        weight=float(request.form.get("weight"))
+        from_unit=request.form["from"]
+        to_unit=request.form["to"]
+        # Using gram as the base unit allows conversion between any two units
+        # with a single formula: value * from_factor / to_factor
+        result=f"{weight*weight_factors[from_unit]/weight_factors[to_unit]} {to_unit}"
+        return render_template("result.html",result=result)
     
 @app.route("/temperature-conversion",methods=["GET","POST"])
 def temp():
